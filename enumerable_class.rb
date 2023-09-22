2b1812ee07f88f6a316d6a9def9f4054131a3843
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-22 18:23:41 +0300

    Add the module for enumerables

--- enumerable_class.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# A custom class for enumerables
-class MyList
-  def self.my_enumerable
-    puts 'working'
-  end
-end
---
+++ enumerable_module.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# A custom module for enumerables
+module MyEnumerable
+  def all?
+    each { |element| return false unless yield(element) }
+    true
+  end
+
+  def any?
+    each { |element| return true if yield(element) }
+    false
+  end
+
+  def filter
+    result = []
+    each { |element| result << element if yield(element) }
+    result
+  end
+end
