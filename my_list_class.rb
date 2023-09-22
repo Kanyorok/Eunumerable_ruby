1c19750a56e38038e8c172bd91c35005dadaa800
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-22 18:39:05 +0300

    Fix linter errors in the code

--- my_list_class.rb
+++ my_list_class.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'enumerable_module'
 
 class MyList
@@ -16,10 +18,10 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? { |e| e < 5 }    
-puts list.all? { |e| e > 5 }    
-puts list.any? { |e| e == 2 }    
-puts list.any? { |e| e == 5 }    
-filtered_list = list.filter { |e| e.even? }  
-puts filtered_list.inspect       
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+filtered_list = list.filter(&:even?)
+puts filtered_list.inspect
 list.each { |e| print "#{e} " }
